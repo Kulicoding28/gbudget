954ea9cd89e1456191c7b6556e31760b9483db0f
zero <ahmadabdulaziz380@gmail.com>
2024-05-29 14:09:44 +0700

    feat:dialog-modal-setup-api-categories

--- app/(dashboard)/_components/CategoryPicker.tsx
+++ app/(dashboard)/_components/CategoryPicker.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { TransactionType } from "@/lib/types";
+import { Category } from "@prisma/client";
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
 
 interface Props {
@@ -8,6 +10,19 @@ interface Props {
 }
 
 function CategoryPicker({ type }: Props) {
+  const [open, setOpen] = React.useState(false);
+  const [value, setValue] = React.useState("");
+
+  const categoriesQuery = useQuery({
+    queryKey: ["categories", type],
+    queryFn: () =>
+      fetch(`/api/categories?type=${type}`).then((res) => res.json()),
+  });
+
+  const selectedCategory = categoriesQuery.data?.find(
+    (category: Category) => category.name === value
+  );
+
   return <div></div>;
 }
 
