6a0b57e8e782224cf10b404295fc3578873f1aa5
zero <ahmadabdulaziz380@gmail.com>
2024-05-30 12:13:50 +0700

    feat:dialog-modal-setup-api-categories

--- app/(dashboard)/_components/CreateCategoryDialog.tsx
+++ app/(dashboard)/_components/CreateCategoryDialog.tsx
@@ -1,6 +1,33 @@
 "use client";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Popover, PopoverTrigger } from "@/components/ui/popover";
 import { TransactionType } from "@/lib/types";
+import { cn } from "@/lib/utils";
+import {
+  CreateCategorySchema,
+  CreateCategorySchemaType,
+} from "@/schema/categories";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { PlusSquare } from "lucide-react";
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 interface Props {
   type: TransactionType;
@@ -8,7 +35,95 @@ interface Props {
 function CreateCategoryDialog({ type }: Props) {
   const [open, setOpen] = useState(false);
 
-  return <div></div>;
+  const form = useForm<CreateCategorySchemaType>({
+    resolver: zodResolver(CreateCategorySchema),
+    defaultValues: {
+      type,
+    },
+  });
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button
+          variant={"ghost"}
+          className="flex border-separate items-center justify-start rounded-none border-b px-3 py-3 text-muted bg-foreground"
+        >
+          <PlusSquare className="mr-2 h-2 w-4" />
+          Create new
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>
+            Create{" "}
+            <span
+              className={cn(
+                "mt-1",
+                type === "income" ? "text-emerald-500" : "text-red-500"
+              )}
+            >
+              {type}
+            </span>{" "}
+            category
+          </DialogTitle>
+          <DialogDescription>
+            Categories are used to group your transactions.
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form className="space-y-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input defaultValue={""} {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    transaction description (optional)
+                  </FormDescription>
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="icon"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Icon</FormLabel>
+                  <FormControl>
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <Button
+                          variant={"outline"}
+                          className="h-[100px] w-full"
+                        >
+                          {form.watch("icon") ? (
+                            <div>Selected icon</div>
+                          ) : (
+                            <div className="flex flex-col items-center gap-2">
+                              No Slection
+                            </div>
+                          )}
+                        </Button>
+                      </PopoverTrigger>
+                    </Popover>
+                  </FormControl>
+                  <FormDescription>
+                    This is how your category will appear in the app
+                  </FormDescription>
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
 }
 
 export default CreateCategoryDialog;
