c01048d2e57c498bd1d300529f6ba2e971f775ee
zero <ahmadabdulaziz380@gmail.com>
2024-06-06 13:30:05 +0700

    make-api-balance

---
+++ app/api/stats/balance/route.ts
@@ -0,0 +1,51 @@
+import prisma from "@/lib/db";
+import { OverviewQuerySchema } from "@/schema/overview";
+import { currentUser } from "@clerk/nextjs/server";
+import { IncomingMessage } from "http";
+import { redirect } from "next/navigation";
+
+export async function GET(request: Request) {
+  const user = await currentUser();
+  if (!user) {
+    redirect("/sign-in");
+  }
+  const { searchParams } = new URL(request.url);
+  const from = searchParams.get("from");
+  const to = searchParams.get("to");
+
+  const queryParams = OverviewQuerySchema.safeParse({ from, to });
+
+  if (!queryParams.success) {
+    return Response.json(queryParams.error, {
+      status: 400,
+    });
+  }
+  const stats = await getBalanceStats(
+    user.id,
+    queryParams.data.from,
+    queryParams.data.to
+  );
+  return Response.json(stats);
+}
+
+export type GetBalanceStatsResponseType = Awaited<
+  ReturnType<typeof getBalanceStats>
+>;
+async function getBalanceStats(userId: string, from: Date, to: Date) {
+  const totals = await prisma.transaction.groupBy({
+    by: ["type"],
+    where: {
+      userId,
+      date: {
+        gte: from,
+        lte: to,
+      },
+    },
+    _sum: { amount: true },
+  });
+
+  return {
+    expanse: totals.find((t) => t.type === "expanse")?._sum.amount || 0,
+    income: totals.find((t) => t.type === "income")?._sum.amount || 0,
+  };
+}
