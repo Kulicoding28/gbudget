f07c2b751e8ee31d1b52464b008b70c01509d48a
zero <ahmadabdulaziz380@gmail.com>
2024-06-06 12:37:27 +0700

    feat:add-overviewmenu-slicing-&&-function

---
+++ app/(dashboard)/_components/Overview.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { DateRangePicker } from "@/components/ui/date-range-picker";
+import { MAX_DATE_RANGE_DAYS } from "@/lib/constant";
+import { UserSettings } from "@prisma/client";
+import { differenceInDays, setDate, startOfMonth } from "date-fns";
+import React, { Fragment, useState } from "react";
+import { toast } from "sonner";
+
+function Overview({ userSettings }: { userSettings: UserSettings }) {
+  const [dateRange, SetDateRange] = useState<{ from: Date; to: Date }>({
+    from: startOfMonth(new Date()),
+    to: new Date(),
+  });
+
+  return (
+    <Fragment>
+      <div className="container flex flex-wrap justify-between items-end gap-2 py-6">
+        <h2 className="text-3xl font-bold">Overview</h2>
+        <div className="flex items-center gap-2">
+          <DateRangePicker
+            initialDateFrom={dateRange.from}
+            initialCompareTo={dateRange.to}
+            onUpdate={(values) => {
+              const { from, to } = values.range;
+              //   we updated the date range only if both date are set
+              if (!from || !to) return;
+              if (differenceInDays(to, from) > MAX_DATE_RANGE_DAYS) {
+                toast.error(
+                  `this is selected date range is too big. Max allow range is ${MAX_DATE_RANGE_DAYS} days`
+                );
+                return;
+              }
+              SetDateRange({ from, to });
+            }}
+          />
+        </div>
+      </div>
+    </Fragment>
+  );
+}
+
+export default Overview;
