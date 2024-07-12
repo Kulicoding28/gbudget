cdc2310c8b10ac3b7d50fee89ac6f0477c9c8cbd
zero <kkenzumi@gmail.com>
2024-07-12 11:18:31 +0700

    Commit message

--- app/(dashboard)/_components/HistoryPeriodSelector.tsx
+++ app/(dashboard)/_components/HistoryPeriodSelector.tsx
@@ -51,6 +51,14 @@ function HistoryPeriodSelector({
             years={historyPeriods.data || []}
           />
         </SkeletonWrapper>
+        {timeframe === "month" && (
+          <SkeletonWrapper
+            isLoading={historyPeriods.isFetching}
+            fullWidth={false}
+          >
+            <MonthSelector period={period} setPeriod={setPeriod} />
+          </SkeletonWrapper>
+        )}
       </div>
     </div>
   );
@@ -81,11 +89,48 @@ function YearSelector({
         <SelectValue />
       </SelectTrigger>
       <SelectContent>
-        {years?.map((year) => (
+        {years.map((year) => (
           <SelectItem key={year} value={year.toString()}>
             {year}
           </SelectItem>
         ))}
+      </SelectContent>
+    </Select>
+  );
+}
+
+function MonthSelector({
+  period,
+  setPeriod,
+}: {
+  period: Period;
+  setPeriod: (period: Period) => void;
+}) {
+  return (
+    <Select
+      value={period.month.toString()}
+      onValueChange={(value) =>
+        setPeriod({
+          year: period.year,
+          month: parseInt(value),
+        })
+      }
+    >
+      <SelectTrigger className="w-[180px]">
+        <SelectValue />
+      </SelectTrigger>
+      <SelectContent>
+        {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((month) => {
+          const monthStr = new Date(period.year, month, 1).toLocaleString(
+            "default",
+            { month: "long" }
+          );
+          return (
+            <SelectItem key={month} value={month.toString()}>
+              {monthStr}
+            </SelectItem>
+          );
+        })}
       </SelectContent>
     </Select>
   );
