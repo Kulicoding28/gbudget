e8682565a41b4b6f1ad158cda0a5b108b2f71aec
zero <kkenzumi@gmail.com>
2024-07-16 14:43:43 +0700

    fixing:FE

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -15,7 +15,7 @@ function Logo() {
 export function LogoMobile() {
   return (
     <a href="/" className="flex items-center gap-2">
-      <p className="bg-gradient-to-r from-amber-400 to-orange-500 text-transparent bg-clip-text text-3xl font-bold leading-tight tracking-tighter">
+      <p className="bg-gradient-to-r from-violet-400 to-purple-500 text-transparent bg-clip-text text-3xl font-bold leading-tight tracking-tighter">
         GbudgetTracker
       </p>
     </a>
