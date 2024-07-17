4ee03a221b2274068b38c3cc184fd8e13d17fb0a
zero <kkenzumi@gmail.com>
2024-07-17 13:39:49 +0700

    feat:addPageManage-Done

---
+++ app/(dashboard)/_components/DeleteCategoryDialog.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { Category } from "@prisma/client";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { ReactNode } from "react";
+import { DeleteCategory } from "../_actions/categories";
+import { toast } from "sonner";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { AlertDescription } from "@/components/ui/alert";
+import { TransactionType } from "@/lib/types";
+
+interface Props {
+  trigger: ReactNode;
+  category: Category;
+}
+function DeleteCategoryDialog({ category, trigger }: Props) {
+  const categoryIdentifier = `${category.name}-${category.type}`;
+  const queryClient = useQueryClient();
+
+  const deleteMutation = useMutation({
+    mutationFn: DeleteCategory,
+    onSuccess: async () => {
+      toast.success("Category deleted successfully🥳", {
+        id: categoryIdentifier,
+      });
+
+      await queryClient.invalidateQueries({
+        queryKey: ["categories"],
+      });
+    },
+    onError: () => {
+      toast.error("Failed to delete category", {
+        id: categoryIdentifier,
+      });
+    },
+  });
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>{trigger}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are You Sure ?</AlertDialogTitle>
+          <AlertDescription>
+            This Action cannot be undone. This will permanently delete your
+            category
+          </AlertDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            onClick={() => {
+              toast.loading("Deleting category...", {
+                id: categoryIdentifier,
+              });
+              deleteMutation.mutate({
+                name: category.name,
+                type: category.type as TransactionType,
+              });
+            }}
+          >
+            Continue
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
+
+export default DeleteCategoryDialog;
--- app/(dashboard)/manage/page.tsx
+++ app/(dashboard)/manage/page.tsx
@@ -11,8 +11,13 @@ import {
 } from "@/components/ui/card";
 import { TransactionType } from "@/lib/types";
 import { useQuery } from "@tanstack/react-query";
-import { TrendingDown, TrendingUp } from "lucide-react";
+import { PlusSquare, TrashIcon, TrendingDown, TrendingUp } from "lucide-react";
 import CreateCategoryDialog from "../_components/CreateCategoryDialog";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { cn } from "@/lib/utils";
+import { Category } from "@prisma/client";
+import DeleteCategoryDialog from "../_components/DeleteCategoryDialog";
 
 function page() {
   return (
@@ -57,8 +62,10 @@ function CategoryList({ type }: { type: TransactionType }) {
     queryFn: () =>
       fetch(`/api/categories?type=${type}`).then((res) => res.json()),
   });
+
+  const dataAvailable = categoriesQuery.data && categoriesQuery.data.length > 0;
   return (
-    <SkeletonWrapper isLoading={categoriesQuery.isFetching}>
+    <SkeletonWrapper isLoading={categoriesQuery.isLoading}>
       <Card>
         <CardHeader>
           <CardTitle className="flex items-center justify-between gap-2">
@@ -78,10 +85,66 @@ function CategoryList({ type }: { type: TransactionType }) {
             <CreateCategoryDialog
               type={type}
               SuccessCallback={() => categoriesQuery.refetch()}
+              trigger={
+                <Button className="gap-2 text-sm">
+                  <PlusSquare className="h-4 w-4" />
+                  Create category
+                </Button>
+              }
             />
           </CardTitle>
         </CardHeader>
+        <Separator />
+        {!dataAvailable && (
+          <div className="flex h-40 w-full flex-col items-center justify-center">
+            <p>
+              No
+              <span
+                className={cn(
+                  "m-1",
+                  type === "income" ? "text-emerald-500" : "text-red-500"
+                )}
+              >
+                {type}
+              </span>
+              categories yet
+            </p>
+            <p className="text-sm text-muted-foreground">
+              Create one get started
+            </p>
+          </div>
+        )}
+        {dataAvailable && (
+          <div className="grid grid-flow-row gap-2 p-2 sm:grid-flow-row sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            {categoriesQuery.data.map((category: Category) => (
+              <CategoryCard category={category} key={category.name} />
+            ))}
+          </div>
+        )}
       </Card>
     </SkeletonWrapper>
+  );
+}
+
+function CategoryCard({ category }: { category: Category }) {
+  return (
+    <div className="flex border-separate flex-col justify-between rounded-md border shadow-md shadow-black/[0.1] dark:shadow-white/[0.1]">
+      <div className="flex flex-col items-center gap-2 p-4">
+        <span>{category.icon}</span>
+        <span>{category.name}</span>
+      </div>
+      <DeleteCategoryDialog
+        category={category}
+        trigger={
+          <Button
+            className="flex w-full border-separate items-center gap-2 rounded-t-none text-muted-foreground hover:bg-red-500/20 "
+            variant={"secondary"}
+          >
+            <TrashIcon className="w-4 h-4" />
+            Remove
+          </Button>
+        }
+      />
+    </div>
   );
 }
